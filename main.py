e91c02fed40d62514355f86463063fe5f644ef6a
Fábio Adler <adlerluna@hotmail.com>
2026-06-08 13:00:44 -0300

    v2.0 beta [2]

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from rich.console import Console
+from rich.panel import Panel
 from bs4 import BeautifulSoup as bs
 import requests,sys,socket,queue
 
@@ -7,21 +8,23 @@ console = Console()
 class CIAI():
     def __init__(self,host:str):
         self.__host=host
-        self.__logo = """
+        self.logo = """
  ██████╗██╗ █████╗ ██╗    ██████╗     ██████╗ 
 ██╔════╝██║██╔══██╗██║    ╚════██╗   ██╔═████╗
 ██║     ██║███████║██║     █████╔╝   ██║██╔██║
 ██║     ██║██╔══██║██║    ██╔═══╝    ████╔╝██║
 ╚██████╗██║██║  ██║██║    ███████╗██╗╚██████╔╝
  ╚═════╝╚═╝╚═╝  ╚═╝╚═╝    ╚══════╝╚═╝ ╚═════╝ 
- by: Fábio Adler de Luna Gomes GitHub: https://github.com/fabioadler
+ by: [bold]Fábio Adler de Luna Gomes[/bold] GitHub: [bold]https://github.com/fabioadler[/bold]
         """
         self.__urls=['https://www.whois.com/search.php?query=','https://ipapi.com/ip_api.php?ip=','https://tools.keycdn.com/geo?host=']
         self.__headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.26','content-type':'application/x-www-form-urlencoded'}
         self.__whois__()
         self.__ipapi__()
-        self.resultado_final = f"{self.__logo}\n\n{self.__whois__result}\n\n{self.__ipapi__result}\n\n  Maps: https://www.google.com/maps/@{self.__latitude},{self.__longitude},13.00z\n\n"
-        print(self.resultado_final)
+        self.resultado_final = f"\n\n{self.__whois__result}\n\n{self.__ipapi__result}\n\n  Maps: https://www.google.com/maps/@{self.__latitude},{self.__longitude},13.00z\n\n"
+        self.console = Console()
+        self.console.print(Panel(self.logo,title="CIAI",border_style="green"))
+        self.console.print(self.resultado_final)
 
     def __whois__(self):
         pag = bs(requests.get(url=f"{self.__urls[0]}{self.__host}",headers=self.__headers).text,'html.parser')
@@ -54,5 +57,7 @@ class CIAI():
                 for local in pag[key].keys():
                     self.__ipapi__result += f"   {local}: {pag[key][local]}\n"
 
-host = str(input("Digite o host: "))
-ciai = CIAI(host=host)
+if(len(sys.argv) > 2 and "-h" in sys.argv):
+    ciai = CIAI(host=sys.argv[sys.argv.index("-h") + 1])
+else:
+    print("Forma de usar python3 main.py -h [host]")
